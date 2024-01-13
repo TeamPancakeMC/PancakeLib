956558ce7e8d7d007e2990e59102bc05434370d4
XiaoHuNao <shiwenbin2250@gmail.com>
2024-01-13 23:46:52 +0800

    Create

---
+++ src/main/java/com/pancake/pancake_lib/PancakeLib.java
@@ -0,0 +1,20 @@
+package com.pancake.pancake_lib;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(PancakeLib.MOD_ID)
+public class PancakeLib {
+    public static final String MOD_ID = "pancake_lib";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public PancakeLib() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
