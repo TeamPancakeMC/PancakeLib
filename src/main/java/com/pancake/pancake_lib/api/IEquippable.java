01358699c5f04f0fcc4216bd8d26ebec79f36f70
XiaoHuNao <shiwenbin2250@gmail.com>
2024-03-27 13:39:10 +0800

    add Warp

--- src/main/java/com/pancake/pancake_lib/api/IEquippable.java
+++ src/main/java/com/pancake/pancake_lib/api/IEquippable.java
@@ -1,13 +1,14 @@
 package com.pancake.pancake_lib.api;
 
 
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.crafting.Ingredient;
 
 
 
 public interface IEquippable<T> {
-    boolean checkEquippable(Player player,Ingredient ingredient);
+    boolean checkEquippable(LivingEntity livingEntity, Ingredient ingredient);
 
     T getSlotType();
 }
